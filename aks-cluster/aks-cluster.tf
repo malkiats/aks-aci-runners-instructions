c157effafc584159b3976eab5cb2faa4e44e8d38
malkiats <msjanjua@live.co.uk>
2025-12-29 20:17:25 +0800

    updated aks terraform code

--- aks-cluster/aks-cluster.tf
+++ aks-cluster/aks-cluster.tf
@@ -11,11 +11,16 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
     vm_size              = "Standard_D2_v2"
     orchestrator_version = data.azurerm_kubernetes_service_versions.current.latest_version
 
-    min_count            = 1
-    max_count            = 3
+    # enable autoscaling and capacity
+    enable_auto_scaling = true
+    min_count           = 1
+    max_count           = 3
 
     os_disk_size_gb = 30
     type            = "VirtualMachineScaleSets"
+
+    # place AKS node pool into the Azure CNI subnet created above
+    vnet_subnet_id = azurerm_subnet.aks_subnet.id
 
     node_labels = {
       "nodepool-type" = "system"
@@ -36,12 +41,17 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
     log_analytics_workspace_id = azurerm_log_analytics_workspace.insights.id
   }
 
-  # Enable RBAC and configure Azure AD admin group via the provider-supported block
+  # Enable RBAC and configure Azure AD admin group
   role_based_access_control_enabled = true
 
   azure_active_directory_role_based_access_control {
     admin_group_object_ids = [azuread_group.aks_administrators.object_id]
-    # tenant_id or azure_rbac_enabled can be added here if needed
+  }
+
+  # Virtual Nodes (ACI) configuration - reference the ACI subnet name
+  aci_connector_linux {
+    # this must be the subnet name (string) that has the ACI delegation
+    subnet_name = azurerm_subnet.aci_subnet.name
   }
 
   # Windows Profile - do not hardcode passwords in VCS. Provide via tfvars or CI secret.
---
+++ aks-cluster/aks-virtual-nodes.tf
@@ -0,0 +1,31 @@
+resource "azurerm_virtual_network" "vnet" {
+  name                = "${local.cluster_name}-vnet"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  address_space       = ["10.0.0.0/16"]
+  tags                = local.common_tags
+}
+
+# Subnet for AKS node pools (Azure CNI)
+resource "azurerm_subnet" "aks_subnet" {
+  name                 = "aks-subnet"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.0.0/22"] # adjust for expected node+pod IPs
+}
+
+# Subnet for ACI (Virtual Nodes) - must have container instance delegation
+resource "azurerm_subnet" "aci_subnet" {
+  name                 = "aci-subnet"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.1.0.0/24"] # sized for expected virtual node usage
+
+  delegation {
+    name = "aciDelegation"
+    service_delegation {
+      name    = "Microsoft.ContainerInstance/containerGroups"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
+    }
+  }
+}
